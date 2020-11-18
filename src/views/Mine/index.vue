d46b56c7f0659148b4a28791f32a0f7701a1b2ad
username <email@example.com>
2020-11-18 12:34:47 +0800

    add creareComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="main">
+        <Header title="喵喵登录"></Header>
+        <Login></Login>
+        <Tabbar></Tabbar>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header/header.vue'
+import Tabbar from '@/components/Tabbar/tabbar.vue'
+import Login from '@/components/Login'
+
+export default {
+    name: 'Mine',
+    components:{
+        Header,
+        Tabbar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
